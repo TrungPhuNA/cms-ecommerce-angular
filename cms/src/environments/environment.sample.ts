3bb71262fdcd5de1cdc96f37da408be327768122
Trinh Xuan Le <letx.1502@gmail.com>
2024-09-01 23:50:04 +0700

    init category, auth, profile

--- cms/src/environments/environment.sample.ts
+++ cms/src/environments/environment.sample.ts
@@ -1,7 +1,8 @@
-export const domain = 'http://localhost:4201';
+export const domain = 'http://localhost:4222';
 export const environment = {
   production: true,
   appVersion: '1.0.0',
-  apiUrl: '',
-  domain: domain
+  apiUrl: 'http://localhost:8080/',
+  domain: domain,
+  tokenField: 'jwt_cms_ecommerce_v1',
 };
