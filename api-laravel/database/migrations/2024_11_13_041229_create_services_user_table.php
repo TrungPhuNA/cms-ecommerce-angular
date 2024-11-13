7b955a030c9bc82f17ace7ec5a6ff30fdc22300c
phudev <codethue94@gmail.com>
2024-11-13 11:14:45 +0700

    add migrate

---
+++ api-laravel/database/migrations/2024_11_13_041229_create_services_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services_user', function (Blueprint $table) {
+            $table->id();
+            $table->integer("user_id")->index();
+            $table->integer("service_id")->index();
+            $table->integer("price")->default(0);
+            $table->enum("status", ["pending", "processing", "completed", "canceled"])->default("pending");
+            $table->string("name")->nullable();
+            $table->boolean("is_home_service")->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services_user');
+    }
+};
